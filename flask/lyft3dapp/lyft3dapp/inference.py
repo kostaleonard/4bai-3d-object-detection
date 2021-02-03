2a7cb782ea8addcff9dfecd00dc29769416f3039
Leo Kosta <kostaleonard@gmail.com>
2021-02-03 14:23:42 -0500

    Added flask template
    Signed-off-by: Leo Kosta <kostaleonard@gmail.com>

--- flask/lyft3dapp/lyft3dapp/inference.py
+++ flask/lyft3dapp/lyft3dapp/inference.py
@@ -1,12 +1,18 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import os
+
+#model_3d = tf.keras.models.load_model(model, custom_objects={'orientation_loss': orientation_loss})
 
 
 def read_image(image_file):
     return mpimg.imread(image_file)
 
 
-def predict_on_image(image_file):
+def predict_on_image_3d(image_file, cam_config_file):
     image = read_image(image_file)
     print(type(image))
     print(image.shape)
+    root_dir = os.path.dirname(os.path.abspath(__file__))
+    path = os.path.join(root_dir + '/static/images/output.png')
+    plt.imsave(path, image)
--- flask/lyft3dapp/lyft3dapp/routes.py
+++ flask/lyft3dapp/lyft3dapp/routes.py
@@ -1,7 +1,7 @@
 from flask import request, render_template
 from flask import current_app as app
 
-from .inference import predict_on_image
+from .inference import predict_on_image_3d
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -10,10 +10,13 @@ def index():
         return render_template('index.html')
     if request.method == 'POST':
         print('TODO inference')
-        if 'file' not in request.files:
-            print('File Not Uploaded')
+        if 'image_file' not in request.files:
+            print('Image file not uploaded')
             return
-        file = request.files['file']
-        print(type(file))
-        predict_on_image(file)
-        return render_template('index.html')
+        if 'cam_config_file' not in request.files:
+            print('Camera configuration file not uploaded')
+            return
+        image_file = request.files['image_file']
+        config_file = request.files['cam_config_file']
+        predict_on_image_3d(image_file, config_file)
+        return render_template('result.html')
