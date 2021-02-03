195c1546858baf2b9be3065aa50022b663c56668
Leo Kosta <kostaleonard@gmail.com>
2021-02-03 15:46:29 -0500

    Model prediction in flask works
    Signed-off-by: Leo Kosta <kostaleonard@gmail.com>

---
+++ flask/lyft3dapp/lyft3dapp/dataset.py
@@ -0,0 +1,270 @@
+"""This module contains functions for interfacing with the dataset."""
+
+# Import statements.
+import os
+import numpy as np
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import cv2
+from typing import Dict, Any, List, Optional, Tuple
+from math import sin, cos
+from shutil import copyfile, rmtree
+
+# Global constants.
+KITTI_ROOT_DIR = os.path.join('..', 'data', 'KITTI')
+LEFT_CAM_ROOT_DIR = os.path.join(KITTI_ROOT_DIR, 'data_object_image_2')
+TRAIN_IMAGE_DIR = os.path.join(LEFT_CAM_ROOT_DIR, 'training', 'image_2')
+TEST_IMAGE_DIR = os.path.join(LEFT_CAM_ROOT_DIR, 'testing', 'image_2')
+LABEL_DIR = os.path.join(KITTI_ROOT_DIR, 'training', 'label_2')
+PARTITION_LABEL_DIR = os.path.join(KITTI_ROOT_DIR, 'training',
+                                   'label_2_partitioned')
+TRAIN_CALIB_DIR = os.path.join(KITTI_ROOT_DIR, 'data_object_calib', 'training',
+                               'calib')
+TEST_CALIB_DIR = os.path.join(KITTI_ROOT_DIR, 'data_object_calib', 'testing',
+                              'calib')
+PREDICTION_2D_BOX_DIR = os.path.join('..', 'data', 'predictions', '2d_boxes',
+                                     'train')
+PREDICTION_3D_BOX_DIR_TRAIN = os.path.join('..', 'data', 'predictions',
+                                           '3d_boxes', 'train')
+PREDICTION_3D_BOX_DIR_VAL = os.path.join('..', 'data', 'predictions',
+                                         '3d_boxes', 'val')
+PREDICTION_3D_BOX_DIR_TEST = os.path.join('..', 'data', 'predictions',
+                                          '3d_boxes', 'test')
+PROJ_MTX_SHAPE = (3, 4)
+DEFAULT_DATASET_ARGS = {'val_split': 0.2,
+                        'test_split': 0.1,
+                        'split_seed': 2017}
+TRAIN_KEY = 'train'
+VAL_KEY = 'val'
+TEST_KEY = 'test'
+# The 4 rows are, respectively: front, left, back, and right.
+FACE_IDX = np.array([[0, 1, 5, 4],
+                     [1, 2, 6, 5],
+                     [2, 3, 7, 6],
+                     [3, 0, 4, 7]])
+# Occlusion is represented as an int in the interval [0, 4].
+# The occlusion code is used as an index to get the color.
+# Green: not occluded.
+# Yellow: partly occluded.
+# Red: fully occluded.
+# White: unknown.
+OCCLUSION_COLORS = ['green', 'yellow', 'red', 'white']
+
+
+def load_image_into_numpy_array(
+        path: str,
+        target_size: Optional[Tuple[int, int]] = None) -> np.ndarray:
+    """Load an image from file into a numpy array.
+    :param path: The path to the image.
+    :param target_size: If specified, the width and height of the output array.
+    If None, the image is size is unchanged.
+    :return: The image contents as an np.ndarray of type uint8.
+    """
+    img_data = cv2.imread(path)
+    img_data = cv2.cvtColor(img_data, cv2.COLOR_BGR2RGB)
+    if target_size:
+        img_data = cv2.resize(img_data, target_size)
+    return img_data.astype(np.uint8)
+
+
+def display_image(filename: str,
+                  fig_size: Tuple[float, float] = (9.0, 2.75)) -> None:
+    """Displays the image at filename.
+    :param filename: The path to the image.
+    :param fig_size: The size of the output, in inches. The default values will
+    show the KITTI images in the correct aspect ratio without passing the 80
+    character vertical margin in jupyter.
+    """
+    img = load_image_into_numpy_array(filename)
+    plt.imshow(img)
+    fig = plt.gcf()
+    fig.set_size_inches(*fig_size)
+    plt.tight_layout()
+    plt.xlim((0, img.shape[1]))
+    plt.ylim((img.shape[0], 0))
+
+
+def display_image_2d_boxes(filename: str,
+                           image_labels: List[str],
+                           fig_size: Tuple[float, float] = (9.0, 2.75),
+                           force_box_color: Optional[str] = None) -> None:
+    """Displays the image at filename with its 2D bounding boxes.
+    :param filename: The path to the image.
+    :param image_labels: The image's labels.
+    :param fig_size: The size of the output, in inches.
+    :param force_box_color: If specified, all boxes will be of this color. Used
+    mainly for predictions, in which no occlusion information is computed.
+    """
+    display_image(filename, fig_size=fig_size)
+    ax = plt.gca()
+    for line in image_labels:
+        obj = get_label_obj(line)
+        width = obj['x2'] - obj['x1']
+        height = obj['y2'] - obj['y1']
+        color = force_box_color if force_box_color else \
+            OCCLUSION_COLORS[obj['occlusion']]
+        linestyle = '-' if obj['truncation'] <= 0.1 else '--'
+        rect = patches.Rectangle((obj['x1'], obj['y1']), width, height,
+                                 linewidth=3, edgecolor=color,
+                                 facecolor='none', linestyle=linestyle)
+        ax.add_patch(rect)
+
+
+def display_image_3d_boxes(filename: str,
+                           image_labels: List[str],
+                           image_calib: List[str],
+                           fig_size: Tuple[float, float] = (9.0, 2.75),
+                           force_box_color: Optional[str] = None) -> None:
+    """Displays the image at filename with its 3D bounding boxes.
+    :param filename: The path to the image.
+    :param image_labels: The image's labels.
+    :param image_calib: The image's calibration.
+    :param fig_size: The size of the output, in inches.
+    :param force_box_color: If specified, all boxes will be of this color. Used
+    mainly for predictions, in which no occlusion information is computed.
+    """
+    display_image(filename, fig_size=fig_size)
+    ax = plt.gca()
+    P2 = get_projection_matrix(image_calib)
+    for line in image_labels:
+        obj = get_label_obj(line)
+        corners = compute_box_3d(obj, P2)
+        orientation = compute_orientation_3d(obj, P2)
+        if corners is not None and orientation is not None:
+            color = force_box_color if force_box_color else \
+                OCCLUSION_COLORS[obj['occlusion']]
+            linestyle = '-' if obj['truncation'] <= 0.1 else '--'
+            for face in range(4):
+                ax.plot(corners[0, FACE_IDX[face, :]],
+                        corners[1, FACE_IDX[face, :]],
+                        linewidth=3,
+                        color=color,
+                        linestyle=linestyle)
+            ax.plot(orientation[0, :], orientation[1, :], color='white',
+                    linewidth=4)
+            ax.plot(orientation[0, :], orientation[1, :], color='black',
+                    linewidth=2)
+
+
+def get_projection_matrix(image_calib: List[str]) -> np.ndarray:
+    """Returns the projection matrix (P2 for the left monocular camera) listed
+    in the calibration file.
+    :param image_calib: The image's calibration.
+    :return: The projection matrix, P2.
+    """
+    # Example P2 entry.
+    # P2: 7.215377000000e+02 0.000000000000e+00 6.095593000000e+02 ...
+    for line in image_calib:
+        if line.startswith('P2:'):
+            nums = line.split(' ')[1:]
+            P2 = np.array([float(num) for num in nums])
+            return np.reshape(P2, PROJ_MTX_SHAPE)
+    raise ValueError('Could not find an entry for P2 in calibration.')
+
+
+def get_label_obj(label_line: str) -> Dict[str, Any]:
+    """Returns a dict with all of the label properties.
+    :param label_line: A line from a label file.
+    :return: A dict representing the label.
+    """
+    label_arr = label_line.split(' ')
+    obj = {}
+    obj['type'] = label_arr[0]
+    obj['truncation'] = float(label_arr[1])
+    obj['occlusion'] = int(label_arr[2])
+    obj['alpha'] = float(label_arr[3])
+    obj['x1'] = float(label_arr[4])
+    obj['y1'] = float(label_arr[5])
+    obj['x2'] = float(label_arr[6])
+    obj['y2'] = float(label_arr[7])
+    obj['height'] = float(label_arr[8])
+    obj['width'] = float(label_arr[9])
+    obj['length'] = float(label_arr[10])
+    obj['tx'] = float(label_arr[11])
+    obj['ty'] = float(label_arr[12])
+    obj['tz'] = float(label_arr[13])
+    obj['ry'] = float(label_arr[14])
+    return obj
+
+
+def project_to_image(pts_3d: np.ndarray, P: np.ndarray) -> np.ndarray:
+    """Projects the 3D points into 2D using the projection matrix P.
+    :param pts_3d: The 3D points as a 3 x n matrix.
+    :param P: The projection matrix, of shape PROJ_MTX_SHAPE (i.e., 3 x 4).
+    :return: The 2D points as a 2 x n matrix.
+    """
+    # Project in image.
+    pts_3d_ones = np.concatenate((pts_3d, np.ones((1, pts_3d.shape[1]))),
+                                 axis=0)
+    pts_2d = np.matmul(P, pts_3d_ones)
+    # Scale projected points.
+    pts_2d[0, :] = pts_2d[0, :] / pts_2d[2, :]
+    pts_2d[1, :] = pts_2d[1, :] / pts_2d[2, :]
+    pts_2d = pts_2d[:2, :]
+    return pts_2d
+
+
+def compute_box_3d(obj: Dict[str, Any], P: np.ndarray) -> Optional[np.ndarray]:
+    """Returns the 2D corners representing the object's 3D bounding box.
+    :param obj: The label object.
+    :param P: The projection matrix, of shape PROJ_MTX_SHAPE (i.e., 3 x 4).
+    :return: The 2D corners of the object's 3D bounding box, of shape 2 x 8, or
+    None if the bounding box is not in front of the camera.
+    """
+    # Rotation matrix around yaw axis.
+    R = [[+cos(obj['ry']), 0, +sin(obj['ry'])],
+         [0, 1, 0],
+         [-sin(obj['ry']), 0, +cos(obj['ry'])]]
+    R = np.array(R)
+    # 3D bounding box dimensions and corners.
+    l = obj['length']
+    w = obj['width']
+    h = obj['height']
+    x_corners = [l / 2, l / 2, -l / 2, -l / 2, l / 2, l / 2, -l / 2, -l / 2]
+    y_corners = [0, 0, 0, 0, -h, -h, -h, -h]
+    z_corners = [w / 2, -w / 2, -w / 2, w / 2, w / 2, -w / 2, -w / 2, w / 2]
+    corners_centered = np.array([x_corners, y_corners, z_corners])
+    # Rotate and translate 3D bounding box.
+    corners_3d = np.matmul(R, corners_centered)
+    corners_3d[0, :] = corners_3d[0, :] + obj['tx']
+    corners_3d[1, :] = corners_3d[1, :] + obj['ty']
+    corners_3d[2, :] = corners_3d[2, :] + obj['tz']
+    # Only draw 3D bounding box for objects in front of the camera.
+    if np.any(corners_3d[2, :] < 0.1):
+        return None
+    # Project the 3D bounding box into the image plane.
+    corners_2d = project_to_image(corners_3d, P)
+    return corners_2d
+
+
+def compute_orientation_3d(obj: Dict[str, Any], P: np.ndarray) -> \
+        Optional[np.ndarray]:
+    """Returns the 2D points representing the object's orientation.
+    :param obj: The label object.
+    :param P: The projection matrix, of shape PROJ_MTX_SHAPE (i.e., 3 x 4).
+    :return: The 2D points representing the object's orientation, of shape
+    2 x 2, or None if the object is not in front of the camera.
+    """
+    # Rotation matrix around yaw axis.
+    R = [[+cos(obj['ry']), 0, +sin(obj['ry'])],
+         [0, 1, 0],
+         [-sin(obj['ry']), 0, +cos(obj['ry'])]]
+    R = np.array(R)
+    # Orientation in object coordinate system.
+    orientation_3d = [[0.0, obj['length']],
+                      [0.0, 0.0],
+                      [0.0, 0.0]]
+    orientation_3d = np.array(orientation_3d)
+    # Rotate and translate orientation vector.
+    orientation_3d = np.matmul(R, orientation_3d)
+    orientation_3d[0, :] = orientation_3d[0, :] + obj['tx']
+    orientation_3d[1, :] = orientation_3d[1, :] + obj['ty']
+    orientation_3d[2, :] = orientation_3d[2, :] + obj['tz']
+    # Only draw orientation for objects in front of the camera.
+    if np.any(orientation_3d[2, :] < 0.1):
+        return None
+    # Project the 3D orientation vector into the image plane.
+    orientation_2d = project_to_image(orientation_3d, P)
+    return orientation_2d
--- flask/lyft3dapp/lyft3dapp/inference.py
+++ flask/lyft3dapp/lyft3dapp/inference.py
@@ -1,8 +1,43 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import os
+import tensorflow as tf
+import numpy as np
+import cv2
+from .dataset import display_image_3d_boxes
 
-#model_3d = tf.keras.models.load_model(model, custom_objects={'orientation_loss': orientation_loss})
+
+def orientation_loss(y_true: np.ndarray, y_pred: np.ndarray) -> tf.Tensor:
+    """Returns the orientation loss from the predictions.
+    :param y_true: The true orientation, a tensor of shape (None, 2, 2).
+    :param y_pred: The predicted orientation of the same shape.
+    :return: The orientation loss as a tensor.
+    """
+    anchors = tf.reduce_sum(tf.square(y_true), axis=2)
+    anchors = tf.greater(anchors, tf.constant(0.5))
+    anchors = tf.reduce_sum(tf.cast(anchors, tf.float32), 1)
+    loss = (y_true[:, :, 0] * y_pred[:, :, 0] + y_true[:, :, 1] * \
+            y_pred[:, :, 1])
+    loss = tf.reduce_sum((2 - 2 * tf.reduce_mean(loss, axis=0))) / anchors
+    return tf.reduce_mean(loss)
+
+
+def get_model_3d():
+    return tf.keras.models.load_model(MODEL_FILE, custom_objects={'orientation_loss': orientation_loss})
+
+
+CURR_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_FILE = os.path.join(CURR_DIR, 'static/model/model_30_epochs_31JAN.h5')
+MODEL_3D = get_model_3d()
+BIN = 2
+NORM_H, NORM_W = 224, 224
+VEHICLES = ['Car', 'Truck', 'Van', 'Tram', 'Pedestrian', 'Cyclist']
+dims_avg = {'Cyclist': np.array([1.73532436, 0.58028152, 1.77413709]),
+            'Van': np.array([2.18928571, 1.90979592, 5.07087755]),
+            'Tram': np.array([3.56092896, 2.39601093, 18.34125683]),
+            'Car': np.array([1.52159147, 1.64443089, 3.85813679]),
+            'Pedestrian': np.array([1.75554637, 0.66860882, 0.87623049]),
+            'Truck': np.array([3.07392252, 2.63079903, 11.2190799])}
 
 
 def read_image(image_file):
@@ -10,9 +45,82 @@ def read_image(image_file):
 
 
 def predict_on_image_3d(image_file, cam_config_file):
-    image = read_image(image_file)
-    print(type(image))
-    print(image.shape)
-    root_dir = os.path.dirname(os.path.abspath(__file__))
-    path = os.path.join(root_dir + '/static/images/output.png')
-    plt.imsave(path, image)
+    npimg = np.fromfile(image_file, np.uint8)
+    img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    image_save_path = os.path.join(CURR_DIR, 'static/images/input.png')
+    prediction_save_path = os.path.join(CURR_DIR, 'static/images/output.png')
+    cam_config_outfile = os.path.join(CURR_DIR, 'static/config/cam.txt')
+    box3d_file = os.path.join(CURR_DIR, 'static/box3d/box3d.txt')
+    # TODO run the model on the image.
+    #img = cv2.imread(image_file)
+    img = img.astype(np.float32, copy=False) / 255.
+    with open(cam_config_outfile, 'wb') as outfile:
+        outfile.write(cam_config_file.read())
+    with open(cam_config_outfile, 'r') as infile:
+        config = infile.readlines()
+    print(config)
+    print('truncating')
+    config = config[7:]
+    print(config)
+    with open(box3d_file, 'w') as box3d:
+        for line in config:
+            line = line.strip().split(' ')
+            truncated = np.abs(float(line[1]))
+            occluded = np.abs(float(line[2]))
+
+            obj = {'xmin': int(float(line[4])),
+                   'ymin': int(float(line[5])),
+                   'xmax': int(float(line[6])),
+                   'ymax': int(float(line[7])),
+                   }
+
+            patch = img[obj['ymin']:obj['ymax'], obj['xmin']:obj['xmax']]
+            patch = cv2.resize(patch, (NORM_H, NORM_W))
+            patch = patch - np.array([[[103.939, 116.779, 123.68]]])
+            patch = np.expand_dims(patch, 0)
+            prediction = MODEL_3D.predict(patch)
+            # Transform regressed angle
+            max_anc = np.argmax(prediction[2][0])
+            anchors = prediction[1][0][max_anc]
+
+            if anchors[1] > 0:
+                angle_offset = np.arccos(anchors[0])
+            else:
+                angle_offset = -np.arccos(anchors[0])
+
+            wedge = 2. * np.pi / BIN
+            angle_offset = angle_offset + max_anc * wedge
+            angle_offset = angle_offset % (2. * np.pi)
+
+            angle_offset = angle_offset - np.pi / 2
+            if angle_offset > np.pi:
+                angle_offset = angle_offset - (2. * np.pi)
+
+            line[3] = str(angle_offset)
+
+            line[14] = angle_offset + np.arctan(
+                float(line[11]) / float(line[13]))
+
+            # Transform regressed dimension
+            if line[0] in VEHICLES:
+                dims = dims_avg[line[0]] + prediction[0][0]
+            else:
+                dims = dims_avg['Car'] + prediction[0][0]
+
+            line = line[:8] + list(dims) + line[11:]
+
+            # Write regressed 3D dim and oritent to file
+            line = ' '.join([str(item) for item in line]) + ' ' + str(
+                np.max(prediction[2][0])) + '\n'
+            box3d.write(line)
+    plt.imsave(image_save_path, img)
+    with open(box3d_file, 'r') as infile:
+        predicted_labels = [line.strip() for line in infile.readlines()]
+    with open(cam_config_outfile, 'r') as infile:
+        config = [line.strip() for line in infile.readlines()]
+    display_image_3d_boxes(image_save_path,
+                           predicted_labels,
+                           config,
+                           force_box_color='orange')
+    plt.savefig(prediction_save_path)
